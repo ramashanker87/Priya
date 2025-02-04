ffda15b34664c2581a3783a6e26d6021cfea144c
admin <rama@gail.com>
2025-02-04 08:44:17 -0800

    DAY11

--- DAY11/student-managament-app/src/main/java/com/priya/app/controller/StudentController.java
+++ DAY11/student-managament-app/src/main/java/com/priya/app/controller/StudentController.java
@@ -1,10 +1,9 @@
 package com.priya.app.controller;
 
 
+import com.priya.app.model.Student;
 import com.priya.app.repository.StudentRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/student")
@@ -14,8 +13,20 @@ public class StudentController {
 
     public StudentController(StudentRepository studentRepository) {
 
-        this.studentRepository = studentRepository;}
-    @GetMapping
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping("/read")
+    public Iterable<Student> read() {
+        return studentRepository.findAll();
+    }
+
+    @PostMapping("/save")
+    public String save(@RequestBody final Student student) {
+        System.out.println("Saving student: " + student);
+        studentRepository.save(student);
+        return "save";
+    }
 
 
 }
