0f1837663fe4f11269ee7a224075c6cc5280b47e
admin <rama@gail.com>
2025-02-04 10:00:53 -0800

    DAY11

--- DAY11/student-managament-app/src/main/java/com/priya/app/model/Student.java
+++ DAY11/student-managament-app/src/main/java/com/priya/app/model/Student.java
@@ -1,8 +1,18 @@
 package com.priya.app.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
 
 @Entity
 @Table(name = "students")
