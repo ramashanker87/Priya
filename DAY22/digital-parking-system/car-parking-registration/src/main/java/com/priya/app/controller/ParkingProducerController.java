a23081a506c79412ed61e6f9b1cb44c3cc1bcbf0
admin <rama@gail.com>
2025-02-19 12:17:30 -0800

    DAY22

--- DAY22/digital-parking-system/car-parking-registration/src/main/java/com/priya/app/controller/ParkingProducerController.java
+++ DAY22/digital-parking-system/car-parking-registration/src/main/java/com/priya/app/controller/ParkingProducerController.java
@@ -4,22 +4,18 @@ import com.priya.app.model.Car;
 import com.priya.app.model.ParkingEnd;
 import com.priya.app.model.ParkingStart;
 import com.priya.app.service.ParkingService;
-import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.Instant;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/parking")
-public class ParkingController {
+public class ParkingProducerController {
     private ParkingService parkingService;
-     public ParkingController(ParkingService parkingService) {
+     public ParkingProducerController(ParkingService parkingService) {
          this.parkingService = parkingService;
      }
-     private static final Logger logger = LoggerFactory.getLogger(ParkingController.class);
+     private static final Logger logger = LoggerFactory.getLogger(ParkingProducerController.class);
     @PostMapping("/start")
     public ParkingStart startParking(@RequestBody Car car, @RequestParam String parkingNo) {
         logger.info("Starting parking for car");
